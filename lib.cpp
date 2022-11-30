33393789ead403c21de38cdc1d2296802031f36a
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-11-30 15:55:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
+int primo(int a) {
+    int b = a - 1;
+    do {
+        int c;
+        c = a % b;
+        if (c == 0 ) {
+            return 1;
+        } else {
+            b--;
+        }
+    } while (b>1);
+    return 0;
 
+}
